8e795840ef3eb1c10e8739a2c46ffdb936fdafa2
Hassan A. Hashim <h3ssan@protonmail.com>
2025-09-26 05:20:45 +0300

    feat: add central settings file with default host and port
    `settings.h` is the central configuration file that used to configure everything, includes:
    - Server
    - Framework itself
    - Limitation
    and more

---
+++ settings.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+using std::string;
+
+namespace Settings {
+    namespace Server {
+        const string HOST = "127.0.0.1";
+        const int PORT = 8000;
+    }
+}
